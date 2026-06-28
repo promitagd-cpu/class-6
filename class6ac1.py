19220d2667b1ff056c1043b29d4ce1e8b2230b05
promitagd-cpu <promitagd@gmail.com>
2026-06-28 10:47:28 +0530

    Add files via upload

---
+++ class6ac1.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 0
+ 
+if a and b and c :
+    print ("all the numbers have boolean value as true")
+else :
+    print ("at least one value has boolean value as false")
+
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0 :
+    print ("either of the numbers is greater than 0")
+else :
+    print ("no number is greater than 0")
+
+if b > 0 or c > 0 :
+    print ("either of the numbers is greater than 0")
+else :
+    print ("no number is greater than 0")
